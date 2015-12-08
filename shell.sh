b13c8f82f4373089d861c27fc97c291a086691a0
Calin Marian <calin.marian@dreamproduction.com>
2015-12-08 19:08:59 +0200

    cli-migrate and custom scripts
    use dreamproduction/cli-migrate
    use custom scripts for handling config import/export, installing the
    site and pulling new code

--- shell.sh
+++ shell.sh
@@ -2,4 +2,4 @@
 
 echo "Starting a new shell at $SHELL with $(pwd)/vendor/bin added to it's path."
 echo "Exit the shell to remove the path directory."
-PATH=$(pwd)/vendor/bin:$PATH $SHELL
+PATH=$(pwd)/vendor/bin:$(pwd)/scripts/custom:$PATH $SHELL
